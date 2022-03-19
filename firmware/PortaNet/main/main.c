5fe7079a97e7e9e5fd8689f43962e354b45f4651
jmaier <joemaier2@gmail.com>
2022-03-19 17:29:59 -0400

    firmware testing

---
+++ firmware/PortaNet/main/main.c
@@ -0,0 +1,8 @@
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+	//xTaskCreate(&helloController, "helloController", 2048, NULL, 5, NULL);
+}
